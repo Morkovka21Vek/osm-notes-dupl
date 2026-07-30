f444454f2b43f0294a19b180e3fb78cf6e01c100
Morkovka21Vek <morkovka21vek@gmail.com>
2026-07-30 22:50:15 +0300

    fix commented notes & style.css

--- src/parser.py
+++ src/parser.py
@@ -14,8 +14,10 @@ with open("notes.bin", "wb") as out:
             for comment in elem.findall("comment"):
                 if comment.attrib.get("action") == "closed":
                     last_closer_uid = comment.attrib["uid"]
-                elif comment.attrib.get("action") == "commented":
+                elif comment.attrib.get("action") == "commented" and comment.attrib.get("uid"):
                     commented = True
+                elif comment.attrib.get("action") == "opened":
+                    continue
 
                 text = (comment.text or "").lower()
                 if any(word in text for word in wordlist.DUPLICATE_STOP_WORDS):
@@ -23,8 +25,6 @@ with open("notes.bin", "wb") as out:
 
             out.write(record.pack(
                 int(elem.attrib["id"]),
-                #int(round(float(elem.attrib["lat"]) * 10_000_000)),
-                #int(round(float(elem.attrib["lon"]) * 10_000_000)),
                 int(float(elem.attrib["lat"]) * 10_000_000),
                 int(float(elem.attrib["lon"]) * 10_000_000),
                 commented,
